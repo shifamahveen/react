1295d6e0bd555a261c253888189261487b4f1c78
shifamahveen <mahveenshifa1611@gmail.com>
2024-09-10 10:40:10 +0530

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState, useMemo } from 'react';
 import './App.css';
-import Translator from './components/Translator';
 
 const App = () => {
+  const [number, setNumber] = useState(0);
+  const [count, setCount] = useState(0);
+
+  function calcNum(num) {
+    console.log("Calculating...");
+    return Math.pow(num, 2);
+  }
+
+  // const result = calcNum(number);
+  const result = useMemo(() => {
+    calcNum(number)
+  }, [number]);
 
   return (
     <>
-      <Translator />
+      <input type='text' value={number} onChange={(e) => setNumber(e.target.value)} />
+      <h1>{result}</h1>
+      <button onClick={() => setCount(prev => prev+1)}>Increment</button>
+      <h1>{count}</h1>
     </>
   )
 }
